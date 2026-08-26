e45dddab1d6dd8c8e07e590634f9a68227d7301c
maksym <maksym@MacBook-Pro-maksym.local>
2026-08-26 15:12:23 +0300

    add fzf + shortkeys

--- zsh/.zsh/external.sh
+++ zsh/.zsh/external.sh
@@ -12,3 +12,20 @@ export NVM_DIR="$HOME/.nvm"
 # <<< conda initialize <<<
 #bun completions
 [ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
+
+# fzf
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
+export FZF_DEFAULT_OPTS='
+  --height 40% --layout=reverse --border
+  --bind ctrl-/:toggle-preview
+'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :200 {}'"
+export FZF_ALT_C_COMMAND='fd --type d --hidden --exclude .git'
+export FZF_ALT_C_OPTS="--preview 'ls -1 {}'"
+export FZF_CTRL_R_OPTS="--reverse --bind 'ctrl-y:execute-silent(pbcopy <<< {2..})+abort'"
+
+if command -v fzf >/dev/null; then
+    source <(fzf --zsh)
+fi
+
